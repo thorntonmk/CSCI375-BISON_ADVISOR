05741a6c55ff97403a7a4f958019c7c8f1fd1006
Komplete101 <106222631+Komplete101@users.noreply.github.com>
2023-12-11 20:21:16 -0500

    Implemented AI and supporting files

---
+++ firebaseconfig.py
@@ -0,0 +1,10 @@
+firebaseConfig = {
+  'apiKey': "AIzaSyC8gT2dIeMFUpbKndK-1X9KRNzApd78sGo",
+  'authDomain': "bison-advisor-c50d5.firebaseapp.com",
+  'databaseURL': "https://bison-advisor-c50d5-default-rtdb.firebaseio.com/",
+  'projectId': "bison-advisor-c50d5",
+  'storageBucket': "bison-advisor-c50d5.appspot.com",
+  'messagingSenderId': "1020695666218",
+  'appId': "1:1020695666218:web:5957f8ed6530283b212d53",
+  'measurementId': "G-8EHGENS193"
+}
